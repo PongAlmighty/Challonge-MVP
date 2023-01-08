a7dd20443b9545925a6c8c3ce2273499f4d537b9
TheMightyPong <6806519+PongAlmighty@users.noreply.github.com>
2023-01-08 19:18:01 +0000

    Added a repeating refresh on the html code as a "temporary" fix... so... it's permanent now.

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ app = Flask(__name__)
 # store the current state of the HTML page
 current_html = ''
 
+
 def update_html():
   global current_html
   try:
     P1Name = 'Player 1'
     P2Name = 'Player 2'
-    
+
     # Retrieve a tournament by its id (or its url).
     tournament = challonge.tournaments.show('z57xc9')
     participants = challonge.participants.index(tournament["id"])
     matches = challonge.matches.index(tournament["id"])
-    
+
     # get list of open matches
     for match in matches:
       if match["state"] == "open":
@@ -34,42 +35,47 @@ def update_html():
           Player1ID = int(match['player1_id'])
           Player2ID = int(match['player2_id'])
           #get player names
-          Player1Info = challonge.participants.show(tournament["id"], Player1ID)
+          Player1Info = challonge.participants.show(tournament["id"],
+                                                    Player1ID)
           Player1Name = Player1Info['name']
-    
-          Player2Info = challonge.participants.show(tournament["id"], Player2ID)
+
+          Player2Info = challonge.participants.show(tournament["id"],
+                                                    Player2ID)
           Player2Name = Player2Info['name']
-          P1Name=(Player1Name)
-          P2Name=(Player2Name)
-  
+          P1Name = (Player1Name)
+          P2Name = (Player2Name)
+
     # format the HTML output using CSS styles
     with app.app_context():
       new_html = render_template('index.html', P1Name=P1Name, P2Name=P2Name)
-      
+
     # update the HTML page if the new HTML is different from the old HTML
     if new_html != current_html:
       current_html = new_html
       print('updating HTML')
       return current_html  # return the updated HTML
-      
-      
+
   except Exception as e:
     print(e)
 
-def update_html_loop():    
-# update the HTML page every 20 seconds
+
+def update_html_loop():
+  # update the HTML page every 20 seconds
   while True:
     update_html()
     time.sleep(20)
-  
+
+
 # start the update loop in a separate thread
 update_thread = Thread(target=update_html_loop)
 update_thread.start()
+
 
 # start the Flask app and run the development web server
 @app.route('/')
 def index():
   print('Accessed index route')
   return current_html
+
 
 app.run(host='0.0.0.0', port=81)
